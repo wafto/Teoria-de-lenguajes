f3bc4b034dfbce37791284292cd5a11bd4c1e4d4
Aftab Hussain Miranda <wafto.mx@gmail.com>
2011-08-21 22:25:35 -0500

    Starting senoidal snippet

---
+++ senoidal/senoidal.c
@@ -0,0 +1,16 @@
+/**
+ * matriz.c
+ * Programa que imprime en pantalla una senoidal con asteriscos.
+ * Elaborado por: Aftab Hussain Miranda
+ * Fecha de elaboracion: 21 de Agosto del 2011.
+ *
+ * Repositorio: https://github.com/wafto
+ * Compilacion: gcc -o app_senoidal senoidal.c
+ * Ejecucion: ./app_senoidal
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    return 0;
+}
